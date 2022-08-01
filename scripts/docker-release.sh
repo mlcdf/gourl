a6dba3ee93daa559d03cc784e55e8402deb9e831
Maxime Le Conte des Floris <git@mlcdf.fr>
2022-08-01 11:19:00 +0200

    misc

--- scripts/docker-release.sh
+++ scripts/docker-release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # https://docs.docker.com/engine/reference/commandline/build/#custom-build-outputs
     
-DOCKER_BUILDKIT=1 docker build -o dist . --network host
+DOCKER_BUILDKIT=1 docker build -o dist .
